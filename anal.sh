0b10ade62a610b22b68ab2047ad06a0ab36f2576
Takumi Matsuura <tm99hjkl@gmail.com>
2023-04-18 14:56:41 +0900

    Improve output format in `anal`

--- anal.sh
+++ anal.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+echo -e "% \tcount \tline"
 file=$1
 unique_line=$(sort $file | uniq)
+ln=$(cat $file | wc -l)
+
+acc=""
 for line in $unique_line; do
     freq=$(grep -o $line $file | wc -l)
-    p=$(echo "scale=4; $freq / 1479" | bc)
-    echo -e "$p, $freq \t$line"
+    p=$(echo "scale=5; $freq / $ln" | bc)
+    acc+=$(echo "$p \t$freq \t$line\n")
 done
+
+echo -e "$(echo -e $acc | sort -r -n)"
