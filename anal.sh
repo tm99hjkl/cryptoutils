ab7ee037081bd4e3ba1ff18dca9e7153f62291de
Takumi Matsuura <tm99hjkl@gmail.com>
2023-04-19 17:16:29 +0900

    Update `anal`

--- anal.sh
+++ anal.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-echo -e "% \tcount \tline"
 file=$1
+
+echo -e "% \tcount \tline"
+
 unique_line=$(sort $file | uniq)
 ln=$(cat $file | wc -l)
 
 acc=""
 for line in $unique_line; do
-    freq=$(grep -o $line $file | wc -l)
-    p=$(echo "scale=5; $freq / $ln" | bc)
-    acc+=$(echo "$p \t$freq \t$line\n")
+    cnt=$(grep -o $line $file | wc -l)
+    p=$(echo "scale=5; $cnt / $ln" | bc)
+    acc+=$(echo "$p \t$cnt \t$line\n")
 done
 
 echo -e "$(echo -e $acc | sort -r -n)"
