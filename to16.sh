64dd38f0174fbdfeb8516068995cf1d4faf13b76
Takumi Matsuura <tm99hjkl@gmail.com>
2023-05-05 14:12:26 +0900

    Change not to output `\` of `to2` and `to16`

--- to16.sh
+++ to16.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 read arg
-echo "obase=16; $arg" | bc
+echo "obase=16; $arg" | bc | tr -d '\\\n'
--- to2.sh
+++ to2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 read arg
-echo "obase=2; $arg" | bc
+echo "obase=2; $arg" | bc | tr -d '\\\n'
